d4f12ea9f285de5f7a32ce15f43e7894eb073ed2
leesn465 <lsm71103186@gmail.com>
2026-03-19 21:53:48 +0900

    로그인 기능 옮기기전 백업

--- src/main/java/com/malgn/auth/JwtAuthenticationFilter.java
+++ src/main/java/com/malgn/auth/JwtAuthenticationFilter.java
@@ -12,7 +12,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import tools.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.util.Map;
